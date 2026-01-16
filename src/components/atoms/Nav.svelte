41cf541bc072357e0b6626a724ed45a526f9f39c
26_Ojasvi <93814820+Ojasvisharma26@users.noreply.github.com>
2026-01-17 00:30:34 +0530

    Update Nav.svelte

--- src/components/atoms/Nav.svelte
+++ src/components/atoms/Nav.svelte
@@ -6,101 +6,58 @@
 	import { page } from '$app/stores';
 	let currentPage = $page.url.pathname;
 
-	async function handleClick() {
-		if (currentPage !== '/') {
-			window.location.href = '/';
-		}
-
+	// Forward close event to parent
+	async function handleClick(event: MouseEvent) {
 		const el = document.querySelector(href);
 		if (!el) return;
-		el.scrollIntoView(true);
+		el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+
+		// Dispatch custom event to parent
+		const customEvent = new CustomEvent('click', { detail: {} });
+		dispatchEvent(customEvent);
 	}
 </script>
 
 <li class:selected={isSelected}>
 	<button on:click={handleClick}>
 		<div class="icon-container">
-			<slot/>
+			<slot />
 		</div>
-		<h5>
-			{section}
-		</h5>
+		<h5>{section}</h5>
 	</button>
 </li>
 
 <style lang="scss">
-	li {
-		text-decoration: none;
-		list-style: none;
-	}
+li { text-decoration: none; list-style: none; }
 
-	button {
-		background-color: transparent;
-		border: none;
-		color: var(--text-secondary);
-		font-size: 1.1rem;
-		user-select: none;
-		display: flex;
-		align-items: center;
-		gap: 0.75rem;
-		padding: 13px 25px;
-		border-radius: 100px;
-		cursor: pointer;
-		transition: background-color 0.3s var(--bezier-one), transform 0.3s var(--bezier-one);
+button {
+	background-color: transparent;
+	border: none;
+	color: var(--text-secondary);
+	font-size: 1.1rem;
+	user-select: none;
+	display: flex; align-items: center; gap: 0.75rem;
+	padding: 13px 25px;
+	border-radius: 100px;
+	cursor: pointer;
+	transition: background-color 0.3s, transform 0.3s;
 
-		&:hover {
-			background-color: var(--elevation-four);
-		}
-	}
+	&:hover { background-color: var(--elevation-four); }
+}
 
-	h5 {
-		transition: all 0.3s var(--bezier-one);
-	}
+h5 { transition: all 0.3s; opacity: 0.8; }
 
-	.icon-container {
-		display: none;
-	}
+.icon-container { display: none; }
 
-	button:hover > h5,
-	.selected h5 {
-		color: var(--text-primary);
-		opacity: 1;
-	}
+button:hover > h5,
+.selected h5 { color: var(--text-primary); opacity: 1; }
 
-
-	h5 {
-		opacity: 0.8;
-	}
-
-	@media screen and (max-width: 868px) {
-
-		h5 {
-			transition-delay: 0.5s;
-		}
-
-		.icon-container {
-			display: block;
-			padding: 6px 24px;
-			border-radius: 100px;
-			margin-bottom: 0.4rem;
-			transition: all 0.5s var(--bezier-one);
-			transition-delay: 0.3s;
-		}
-
-		button {
-			flex-direction: column;
-			gap: 0;
-			font-size: 0.9rem;
-			margin-bottom: 0;
-		}
-
-		button:hover {
-			background-color: transparent;
-		}
-
-		button:hover .icon-container,
-		.selected .icon-container {
-			background-color: var(--accent-opacity);
-		}
-	}
+@media screen and (max-width: 868px) {
+	h5 { transition-delay: 0.5s; }
+	.icon-container { display: block; padding: 6px 24px; border-radius: 100px; margin-bottom: 0.4rem; transition: all 0.5s; }
+	button { flex-direction: column; gap: 0; font-size: 0.9rem; margin-bottom: 0; }
+	button:hover { background-color: transparent; }
+	button:hover .icon-container,
+	.selected .icon-container { background-color: var(--accent-opacity); }
+}
 </style>
